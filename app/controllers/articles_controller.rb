9e7efe6849e57435127ae8d607e6392ea3c1ac6d
Andrej Antas <andrej.antas@gmail.com>
2013-09-12 07:37:48 +0200

    init commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  before_filter :authorize, only: [:edit, :update] 
+  
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to @article, notice: "Article has been updated."
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect_to root_url, notice: "Article removed!"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Blog::Application.routes.draw do
+  root to: 'articles#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :users
+  resources :sessions
+
+  resources :articles
+end
