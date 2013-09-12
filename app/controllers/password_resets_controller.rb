48080662cf7e9fb63fc1188448b95377acd0c910
Andrej Antas <andrej.antas@gmail.com>
2013-09-12 09:21:10 +0200

    stuff from episodes #270 and #274 missing, therefore failing spec

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,12 @@ class PasswordResetsController < ApplicationController
   end
   
   def create
+    user = User.find_by_email(params[:email])
+    user.send_password_reset
     redirect_to root_url, notice: "Email sent with password reset instructions."
+  end
+  
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
   validates_uniqueness_of :email
+  validates_presence_of :password, on: :create
+  
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
+  
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+  
 end
