85654cdb29f6a69f9f3c95ca66f857a2f8c7861e
Andrej Antas <andrej.antas@gmail.com>
2013-09-23 13:52:41 +0200

    secret token forgotten

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Blog::Application.config.secret_token = '67c703e3c3f1728357dce8bb0576009ed5cb28c8c110b45559a03b26bac5bc993faf4a45f7ca872e26378e5ce7ad7f100c028601dce32c4b8067396b823d84b3'
+Blog::Application.config.secret_key_base = '67c703e3c3f1728357dce8bb0576009ed5cb28c8c110b45559a03b26bac5bc993faf4a45f7ca872e26378e5ce7ad7f100c028601dce32c4b8067396b823d84b3'
